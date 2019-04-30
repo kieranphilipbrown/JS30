06a946be897e390c538ee9e42e6a026b81c11d91
kieranphilipbrown <kieranpbrown@gmail.com>
2019-04-30 20:20:30 +0100

    Added fix for nav items and set up drum kit page.

--- src/assets/js/nav-button.js
+++ src/assets/js/nav-button.js
@@ -5,24 +5,26 @@ var navOverlay = document.querySelector(".nav__overlay");
 
 if (navOpenButton) {
     var fadeEffect = function(elem) {
-        if (elem.classList.contains("fade_in")) {
-            elem.classList.remove("fade_in");
-        } else {
-            setTimeout(function() {
-                elem.classList.add("fade_in");
-            }, 600);
-        }
+        elem.forEach(function(el) {
+            if (el.classList.contains("fade_in")) {
+                el.classList.remove("fade_in");
+            } else {
+                setTimeout(function() {
+                    el.classList.add("fade_in");
+                }, 600);
+            }
+        });
     };
 
     navOpenButton.addEventListener("click", function() {
         nav.classList.add("nav_open");
         navOverlay.classList.add("nav__overlay--open");
-        var navItem = document.querySelector(".nav__item");
+        var navItem = document.querySelectorAll(".nav__item");
         fadeEffect(navItem);
     });
 
     navCloseButton.addEventListener("click", function() {
-        var navItem = document.querySelector(".nav__item");
+        var navItem = document.querySelectorAll(".nav__item");
         fadeEffect(navItem);
         setTimeout(function() {
             nav.classList.remove("nav_open");
@@ -31,7 +33,7 @@ if (navOpenButton) {
     });
 
     navOverlay.addEventListener("click", function() {
-        var navItem = document.querySelector(".nav__item");
+        var navItem = document.querySelectorAll(".nav__item");
         fadeEffect(navItem);
         setTimeout(function() {
             nav.classList.remove("nav_open");
